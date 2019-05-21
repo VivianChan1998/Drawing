e74af6c31b6a375e7d69f39d6f90081a8793d50d
Vivian <b05901179@ntu.edu.tw>
2019-05-21 19:15:15 +0800

    fixed picItem

--- src/components/UserPicItem.js
+++ src/components/UserPicItem.js
@@ -3,15 +3,12 @@ import img from '../img/Juice/5.png'
 
 
 class UserPicItem extends Component {
-    click() {
-        console.log("clickkk")
-    }
     render() {
         
         return(
             <div className='User_PicItem' onClick={this.props.onClick}>
                 <div className='User_PicItem_imgcontainer'>
-                    <img src={this.props.source} onClick={() => this.click()}></img>
+                    <img src={this.props.source} ></img>
                 </div>
                 <div className='User_PicItem_text'>
                     <h5>{this.props.title}</h5>
